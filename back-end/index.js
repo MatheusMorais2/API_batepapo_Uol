9f0e4dc9c15e063b9d30735c46342b3913cb5518
Matheus <morais.matheus.web@gmail.com>
2022-01-27 16:09:06 -0300

    Route: get /participants

--- back-end/index.js
+++ back-end/index.js
@@ -36,4 +36,19 @@ server.post("/participants", async (req, res) => {
   }
 });
 
+server.get("/participants", async (req, res) => {
+  try {
+    await mongoClient.connect();
+    db = mongoClient.db("API_batepapo_uol");
+
+    const userList = await db.collection("users").find().toArray();
+    res.status(201).send(userList);
+
+    if (mongoClient) mongoClient.close();
+  } catch {
+    res.status(500).send("Internal server error");
+    if (mongoClient) mongoClient.close();
+  }
+});
+
 server.listen(5000);
